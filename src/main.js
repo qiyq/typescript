b326ac9c8b48d41dc66121e88d93da5f8b169e4b
Qi Yingqiang <jerry.qi@grapecity.com>
2017-12-08 10:56:32 +0800

    typecript

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var greet_1 = require("./greet");
-console.log(greet_1.sayHello('nihao'));
+function showHello(divName, name) {
+    var elt = document.getElementById(divName);
+    elt.innerText = greet_1.sayHello(name);
+}
+showHello("greeting", "TypeScript");
 //# sourceMappingURL=main.js.map
